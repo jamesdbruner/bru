0b097c88da763b758ad9874e12583f3e2b82d3a4
james <jamesdbruner@gmail.com>
2024-03-18 23:08:00 -0600

    chore: adds types

---
+++ types.ts
@@ -0,0 +1,23 @@
+export type DenoPermissionName =
+  | 'env'
+  | 'sys'
+  | 'hrtime'
+  | 'net'
+  | 'ffi'
+  | 'read'
+  | 'run'
+  | 'write'
+
+export type DenoPermissions = {
+  [Key in DenoPermissionName]?: boolean | string[]
+}
+
+export interface ModOptions {
+  compile?: boolean
+  install?: boolean
+}
+
+export interface ModulePermissions {
+  default: DenoPermissions
+  options: ModOptions
+}
