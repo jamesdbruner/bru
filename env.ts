7bbcd1db60d0940c5460ac73b48caa9f63c3e83c
james <jamesdbruner@gmail.com>
2024-03-18 23:07:15 -0600

    chore: adds env and example env

---
+++ env.ts
@@ -0,0 +1,7 @@
+import { config } from 'dotenv'
+
+// Load environment variables from .env file
+config({ path: './.env', export: true, safe: true, allowEmptyValues: true })
+
+export const OPENAI_API_KEY = Deno.env.get('OPENAI_API_KEY') || ''
+export const NAME = Deno.env.get('NAME') || 'bru'
