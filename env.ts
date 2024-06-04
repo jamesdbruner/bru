215fb6e9a2fb5b0544e9d02e03328a4501378a6b
james <jamesdbruner@gmail.com>
2024-06-03 22:43:43 -0600

    chore: formatting

--- env.ts
+++ env.ts
@@ -1,7 +1,12 @@
 import { config } from 'dotenv'
 
 // Load environment variables from .env file
-config({ path: './.env', export: true, safe: true, allowEmptyValues: true })
+config({
+  path: './.env',
+  export: true,
+  safe: true,
+  allowEmptyValues: true,
+})
 
 export const OPENAI_API_KEY = Deno.env.get('OPENAI_API_KEY') || ''
 export const NAME = Deno.env.get('NAME') || 'bru'
